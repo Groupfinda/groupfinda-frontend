24c59e7b59e57053c42e3ceda932f62b8317c49a
tankangliang98@gmail.com <tankangliang98@gmail.com>
2020-05-24 02:04:30 +0800

    Finished initial create screen UI

---
+++ src/components/Create/CardDisplay.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Layout, Text, Card, Icon, useTheme } from "@ui-kitten/components";
+import { StyleSheet } from "react-native";
+
+type Props = {
+  title: string;
+  icon: string;
+};
+
+export const CardDisplay: React.FC<Props> = (props) => {
+  const { title, icon } = props;
+  const theme = useTheme();
+  return (
+    <Card status="primary" style={styles.card} appearance="outline">
+      <Text category="h1" appearance="hint">
+        {title}
+      </Text>
+      <Layout style={styles.center}>
+        <Icon
+          style={styles.icon}
+          fill={theme["color-primary-500"]}
+          name={icon}
+        />
+      </Layout>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    borderRadius: 20,
+    margin: 5,
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "transparent",
+  },
+  icon: {
+    width: 100,
+    height: 100,
+  },
+});
--- src/screens/CreateScreen.tsx
+++ src/screens/CreateScreen.tsx
@@ -1,17 +1,32 @@
 import React from "react";
-import { Layout, Text } from "@ui-kitten/components";
-import { StyleSheet } from "react-native";
+import { Layout, Text, Divider, Icon } from "@ui-kitten/components";
+import { StyleSheet, View } from "react-native";
+import { Header, CardDisplay } from "../components/Create";
 
 type Props = {};
 
 const CreateScreen: React.FC<Props> = (props) => {
   return (
-    <Layout>
-      <Text category="h1">This should be CreateScreen</Text>
+    <Layout style={styles.container}>
+      <Header />
+      <Divider />
+      <Icon name="people" />
+      <Layout style={styles.viewStyle} level="4">
+        <CardDisplay title="Join an event" icon="people" />
+        <CardDisplay title="Create an event" icon="plus-square" />
+      </Layout>
     </Layout>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+  viewStyle: {
+    flex: 1,
+    padding: 5,
+  },
+});
 
 export default CreateScreen;
