e21a39c9efd7e80c5e22526bbd98c527f2bf9136
tankangliang98@gmail.com <tankangliang98@gmail.com>
2020-06-07 14:13:37 +0800

    Wrapped wait with act

--- src/screens/__tests__/QuestionsScreen.test.tsx
+++ src/screens/__tests__/QuestionsScreen.test.tsx
@@ -1,44 +1,44 @@
 import React from "react";
-import { render, cleanup } from "../../../utils/test-utils";
+import { render, cleanup, act } from "../../../utils/test-utils";
 import QuestionsScreen from "../QuestionsScreen";
 import { MockedResponse, wait } from "@apollo/react-testing";
 import { getUserQuestions } from "../../graphql/queries";
 
 afterEach(cleanup);
 
-let dummyQuestions = []
-for (let i=0; i < 300; i++) {
-    dummyQuestions.push({
-        order: i,
-        content: "Dummy Question"
-    })
+let dummyQuestions = [];
+for (let i = 0; i < 300; i++) {
+  dummyQuestions.push({
+    order: i,
+    content: "Dummy Question",
+  });
 }
 
 const mocks: MockedResponse[] = [
-    {
-        request: {
-            query: getUserQuestions
+  {
+    request: {
+      query: getUserQuestions,
+    },
+    result: {
+      data: {
+        getUserProfile: {
+          rangeQuestions: [1, 3, 4, 5, 2].concat(new Array(295).fill(0)),
         },
-        result: {
-            data: {
-                getUserProfile: {
-                    rangeQuestions: [1, 3, 4, 5, 2].concat(new Array(295).fill(0))
-                },
-                getAllRangeQuestions: dummyQuestions
-            }
-        }
-    }
-]
+        getAllRangeQuestions: dummyQuestions,
+      },
+    },
+  },
+];
 
 test("renders original UI correctly", () => {
-    let props: any
-    const tree = render(<QuestionsScreen {...props}/>).toJSON();
-    expect(tree).toMatchSnapshot();
+  let props: any;
+  const tree = render(<QuestionsScreen {...props} />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 test("renders loaded UI correctly", async () => {
-    let props: any
-    const tree = render(<QuestionsScreen {...props}/>, {}, mocks);
-    await wait(0);
-    expect(tree.toJSON()).toMatchSnapshot();
-})
+  let props: any;
+  const tree = render(<QuestionsScreen {...props} />, {}, mocks);
+  await act(async () => await wait(0));
+  expect(tree.toJSON()).toMatchSnapshot();
+});
